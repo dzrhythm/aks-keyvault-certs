3024e4395ce1127910c3e0318314a55aa32d9ab5
David Zientara <david.zientara@appliedis.com>
2020-08-17 17:56:56 -0400

    comments and cleanup

--- AKS-Deploy.ps1
+++ AKS-Deploy.ps1
@@ -11,32 +11,46 @@ $keyVaultName =  "YOUR KEY VAULT NAME"
 $clientID =      "YOUR AAD APPCLIENT ID"
 $clientSecret =  "YOUR AAD APPCLIENT SECRET"
 
+# Login with Azure CLI
 az login
 
-# Resource Group
+# Create Resource Group
 az group create --name "$resourceGroup" --location "$location"
 
-# ACR
+# Create ACR and login
 az acr create --resource-group "$resourceGroup" --name "$acrName" --sku Basic
 az acr login --name "$acrName"
 
 # Docker build
 docker build -f Dockerfile -t aspnet-keyvault .
 
-# Tag and push the image to the ACR
+# Docker tag and push the image to the ACR
 docker tag aspnet-keyvault "$acrName.azurecr.io/aspnet-keyvault"
 docker push "$acrName.azurecr.io/aspnet-keyvault"
 
-# Key Vault
+# As per README.md, first:
+# 1. Create your Key Vault. Update the $keyVaultName accordingly.
+# 2. Create the Azure AD app regsitration; update $clientID accordingly.
+# 3. Import the PFX
+# The command below grants your clientID access to get the Key Vault secrets.
 az keyvault set-policy -n "$keyVaultName" --secret-permissions get --spn "$clientID"
-# (import the PFX as per the README.md)
 
-# AKS
+# Create an AKS cluster
 az aks create --resource-group "$resourceGroup" --name "$aksName" --node-count 2 --generate-ssh-keys --attach-acr "$acrName"
+
+# Get credentials for the AKS cluster
 az aks get-credentials --resource-group "$resourceGroup" --name "$aksName"
+
+# Install teh FlexVol driver for Key Vault
 kubectl create -f https://raw.githubusercontent.com/Azure/kubernetes-keyvault-flexvol/master/deployment/kv-flexvol-installer.yaml
+
+# Set up a credential secret for your Auzre AD app registration (update $clientSecret accordingly)
 kubectl create secret generic kvcreds --from-literal "clientid=$clientID" --from-literal "clientsecret=$clientSecret" --type=azure/kv
+
+# Apply the Kubernetes YAML
 kubectl apply -f k8s-aspnetapp-all-in-one.yaml
+
+# Check status
 kubectl get pods
 kubectl get services
 
--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -6,50 +6,54 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace aspnetapp
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder
-                        .UseStartup<Startup>()
-                        .UseKestrel(options =>
-                        {
-                            options.ConfigureHttpsDefaults(ConfigureHttps);
-                        });
-                });
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder
+						.UseStartup<Startup>()
+						.UseKestrel(options =>
+						{
+							// configure Kestrel with our HTTPS certificate
+							options.ConfigureHttpsDefaults(ConfigureHttps);
+						});
+				});
 
-        private static void ConfigureHttps(HttpsConnectionAdapterOptions options)
-        {
-            try
-            {
-                // When we get the certificate from Key Vault as a secret,
-                // it provides the entire PFX file but without the password.
-                // Since PFX is a binary format and a secret is a string,
-                // it is base64 encoded. So we ready in the base and convert
-                // it to the bytes to initialize the X509Certificate2.
-                var certPath = Environment.GetEnvironmentVariable("HTTPS_CERTIFICATE_PATH");
-                var certString = System.IO.File.ReadAllText(certPath);
-                var certBytes = Convert.FromBase64String(certString);
-                var httpsCert = new X509Certificate2(certBytes);
+		private static void ConfigureHttps(HttpsConnectionAdapterOptions options)
+		{
+			try
+			{
+				// When we get the certificate from Key Vault as a secret,
+				// it provides the entire PFX file but without the password.
+				// Since PFX is a binary format and a secret is a string,
+				// it is base64 encoded. So we read in the text file and convert
+				// it to the bytes to initialize the X509Certificate2.
+				var certPath = Environment.GetEnvironmentVariable("HTTPS_CERTIFICATE_PATH");
+				if (!string.IsNullOrEmpty(certPath))
+				{
+					var certString = System.IO.File.ReadAllText(certPath);
+					var certBytes = Convert.FromBase64String(certString);
+					var httpsCert = new X509Certificate2(certBytes);
 
-                Console.WriteLine($"HTTPS cert Subject:    {httpsCert.Subject}");
-                Console.WriteLine($"HTTPS cert Thumbprint: {httpsCert.Thumbprint}");
+					Console.WriteLine($"HTTPS cert Subject:    {httpsCert.Subject}");
+					Console.WriteLine($"HTTPS cert Thumbprint: {httpsCert.Thumbprint}");
 
-                // set the Kestrel HTTPS certificate
-                options.ServerCertificate = httpsCert;
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine($"unable to load https cert: {ex}");
-                throw;
-            }
-        }
-    }
+					// set the Kestrel HTTPS certificate
+					options.ServerCertificate = httpsCert;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.Error.WriteLine($"unable to load https cert: {ex}");
+				throw;
+			}
+		}
+	}
 }
